a7eada7d2f21dd70445e6a88a3a0865224a5b904
Vidit Pandey <vidit.pandey@metronom-external.com>
2020-07-16 03:16:42 +0200

    stop polling if topology is inactive

--- app/components/Topologies/TopologyLayout.jsx
+++ app/components/Topologies/TopologyLayout.jsx
@@ -9,13 +9,21 @@ import { useInterval } from '../../helper/useInterval'
 
 const MetricsLayout = React.lazy(() => import('../Graphs/DataProcessRateGraph'))
 
-const MAX_POLL_COUNT = 2
+// const MAX_POLL_COUNT = 200
 
 export default function TopologyLayout ({ id }) {
   const { setAppTitle } = useContext(AppBarContext)
   const [topologyData, setTopologyData] = useState({})
   const [shouldPoll, setPolling] = useState(false)
-  const [sameStatus, setSameStatus] = useState(0)
+  // const [sameStatus, setSameStatus] = useState(0)
+
+  const shouldPollContinue = () => {
+    // const inactiveStatus = ['TO_START', 'FINISHED', 'EDITED', 'STOPPED', 'ERROR', 'RUN_ERROR', 'INVALID']
+    const inactiveStatus = ['FINISHED']
+    const topologyInactive = inactiveStatus.indexOf(topologyData.topologyStatus) !== -1
+    if (topologyInactive) return false
+    return true
+  }
 
   useEffect(() => {
     async function getTopologyData (id) {
@@ -28,15 +36,15 @@ export default function TopologyLayout ({ id }) {
   }, [])
 
   useInterval(async () => {
-    if (!shouldPoll) return
-    const { topologyId, topologyStatus } = topologyData
+    if (!shouldPollContinue()) return
+    const { topologyId } = topologyData
     const latestStatus = await getTopologyById({ topologyId })
     if (!isEmpty(latestStatus)) setTopologyData(latestStatus)
-    else setPolling(false)
-    if (latestStatus && topologyStatus === latestStatus.topologyStatus) setSameStatus(sameStatus + 1)
-    else setSameStatus(1)
-    if (sameStatus >= MAX_POLL_COUNT) { setPolling(false); setSameStatus(1) }
-  }, shouldPoll ? 3000 : null)
+    // else setPolling(false)
+    // if (latestStatus && topologyStatus === latestStatus.topologyStatus) setSameStatus(sameStatus + 1)
+    // else setSameStatus(1)
+    // if (sameStatus >= MAX_POLL_COUNT) { setPolling(false); setSameStatus(1) }
+  }, shouldPoll ? 2000 : null)
 
   return (
     <div>
