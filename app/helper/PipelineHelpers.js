0e32bc048da1c8ad34cf03932bd8250c0babdfff
Vidit Pandey <vidit.pandey@metronom-external.com>
2020-07-30 19:08:21 +0200

    get fixed color from string for instanceIDs

--- app/helper/PipelineHelpers.js
+++ app/helper/PipelineHelpers.js
@@ -27,18 +27,23 @@ export function parsePipelinesStatusResponse (response) {
   }
 }
 
-function getRandomColor () {
-  var letters = '0123456789ABCD'
-  // var letters = '0123456789ABCDEF'
-  var color = '#'
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return color
-}
-
 export function generateRandomColor (items) {
   const obj = {}
-  items.forEach(i => { obj[i] = getRandomColor() })
+  items.forEach(i => { obj[i] = stringToColour(i) })
   return obj
+}
+
+export function stringToColour (str) {
+  try {
+    var hash = 0
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash)
+    }
+    var colour = '#'
+    for (let i = 0; i < 3; i++) {
+      var value = (hash >> (i * 8)) & 0xFF
+      colour += ('00' + value.toString(16)).substr(-2)
+    }
+    return colour
+  } catch (error) { return '#000000' }
 }
